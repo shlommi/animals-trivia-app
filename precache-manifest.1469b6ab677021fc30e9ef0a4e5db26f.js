ff43b7a035dbdf276f63d1269b3cf09913bfe5ef
shlommi <shlommi@gmail.com>
2019-08-20 19:36:29 +0300

    Updates

---
+++ precache-manifest.1469b6ab677021fc30e9ef0a4e5db26f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8976c1f5d9d0334e0f70ba36e543d7",
+    "url": "/animals-trivia-app/index.html"
+  },
+  {
+    "revision": "90ca7936836c8bada660",
+    "url": "/animals-trivia-app/static/css/main.57ae8f20.chunk.css"
+  },
+  {
+    "revision": "bdc365f2ced34617698f",
+    "url": "/animals-trivia-app/static/js/2.717349fb.chunk.js"
+  },
+  {
+    "revision": "90ca7936836c8bada660",
+    "url": "/animals-trivia-app/static/js/main.3b1749a2.chunk.js"
+  },
+  {
+    "revision": "cd3130b001277cb9e18e",
+    "url": "/animals-trivia-app/static/js/runtime~main.37ea9438.js"
+  }
+]);
